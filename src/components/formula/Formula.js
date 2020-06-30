1a8f1d481c92c8ca208ee9a6142d84c0470df4e5
pylyp <pylyp.kutov@gmail.com>
2020-06-30 23:24:32 +0200

    Add  methods to init and remove listeners from components

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -2,12 +2,26 @@ import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
-
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click']
+    });
+  }
   toHtml() {
     return `
       <div class="info">fx</div>
       <div class="input" contenteditable="true" spellcheck="false"></div>
     `;
+  }
+
+  onInput(event) {
+    console.log(this.$root);
+    console.log('Forumula: onInput', event.target.textContent.trim());
+  }
+
+  onClick(event) {
+    console.log('Formula: onClick');
   }
 }
 
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,6 +3,13 @@ import {ExcelComponent} from '@core/ExcelComponent';
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
 
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click']
+    });
+  }
+
   toHtml() {
     return `
      <div class="button">
@@ -30,5 +37,9 @@ export class Toolbar extends ExcelComponent {
       </div>
 
     `;
+  }
+
+  onClick(event) {
+    console.log(event.target);
   }
 }
