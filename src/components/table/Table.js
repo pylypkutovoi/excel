06168233008fa29968d8d32192b067742d97c179
pylyp <pylyp.kutov@gmail.com>
2020-07-07 22:31:20 +0200

    Add single cell selection logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,9 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {shouldResize} from '@/components/table/table.functions';
+import {isCell, shouldResize} from '@/components/table/table.functions';
+import {TableSelection} from '@/components/table/TableSelection';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -15,9 +17,22 @@ export class Table extends ExcelComponent {
     return createTable(20);
   }
 
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      this.selection.select($target);
     }
   }
 }
